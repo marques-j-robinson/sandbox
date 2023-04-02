9f706a56bdeeecac59983a77de417840e2f0a55b
marques-j-robinson <marques.j.robinson@gmail.com>
2023-04-02 08:31:51 -0700

    lift state and create a game component

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -8,19 +8,9 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const winner = calculateWinner(squares)
-  let status
-  if (winner) {
-    status = `Winnner: ${winner}`
-  } else {
-    status = `Next player: ${(xIsNext ? "X" : "O")}`
-  }
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return
     }
     const nextSquares = squares.slice()
@@ -29,8 +19,15 @@ export default function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
+  }
+  
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = `Winnner: ${winner}`
+  } else {
+    status = `Next player: ${(xIsNext ? "X" : "O")}`
   }
 
   return (
@@ -50,6 +47,28 @@ export default function Board() {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length-1]
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
       </div>
     </div>
   )

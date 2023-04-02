770d81ba74ba6914483e7d291f4aff757504ffab
marques-j-robinson <marques.j.robinson@gmail.com>
2023-04-02 10:36:59 -0700

    create Todo model

--- todo_with_flask/app.py
+++ todo_with_flask/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+
+
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICIATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	complete = db.Column(db.Boolean)
+
 
 @app.route('/')
 def index():
 	return 'Hello, World!'
 
 if __name__ == "__main__":
+	db.create_all()
 	app.run(debug=True)
